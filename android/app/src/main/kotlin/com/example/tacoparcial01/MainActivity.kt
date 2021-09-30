11733ff08173ebc36fa8a3ed6eba3ef532341ce9
Erick Garcia Valenzuela <git@github.com:wuimmholften/titanic.git>
2021-09-29 23:23:52 -0600

    primer commit

---
+++ android/app/src/main/kotlin/com/example/tacoparcial01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tacoparcial01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:tacoparcial01/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'IMC Calculadora',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }}
